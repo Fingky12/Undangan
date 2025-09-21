c9fe706ad4a56fcde14c39456d26c9b7566455c7
Ky.8801 <64500673+Fingky12@users.noreply.github.com>
2025-09-21 14:29:31 +0700

    Berhasil1

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const bukaUndangan = document.getElementById('buka-undangan');
+document.addEventListener("DOMContentLoaded", () => {
+  const bukaBtn = document.querySelector("#buka-undangan"); // tombol "Buka Undangan"
+  const isiUndangan = document.getElementById("isi-undangan");
 
-document.getElementById("bukaUndangan").onclick = function() {
-  alert("Tombol diklik!");
-}
+  // Lock scroll saat awal
+  document.body.classList.add("lock-scroll");
+
+  bukaBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // cegah scroll default
+
+    // Tampilkan section 2
+    isiUndangan.classList.add("active");
+
+    // Buka scroll
+    document.body.classList.remove("lock-scroll");
+
+    // Scroll halus ke section 2
+    isiUndangan.scrollIntoView({ behavior: "smooth" });
+  });
+});
