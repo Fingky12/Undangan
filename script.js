2c700245684da66c419edaf4fa681eec6a835460
Ky.8801 <64500673+Fingky12@users.noreply.github.com>
2025-09-27 20:29:47 +0700

    Update

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const container = document.querySelector('.falling-flowers');
 function createFlower() {
   const flower = document.createElement('div');
   flower.classList.add('flower');
-  flower.innerHTML = '❀'; // bisa diganti gambar bunga PNG/SVG
+  flower.style.backgroundImage = "url('assets/flower1.png')";  // bisa diganti gambar bunga PNG/SVG
+  flower.style.backgroundSize = "cover";
+  flower.style.backgroundRepeat = "no-repeat";
+  flower.style.backgroundSize = "20px";
 
   // posisi random di sumbu X
   flower.style.left = Math.random() * window.innerWidth + 'px';
 
   // ukuran random
-  const size = 15 + Math.random() * 25;
-  flower.style.fontSize = size + 'px';
+  const size = 20 + Math.random() * 40;
+  flower.style.width = size + 'px';
+  flower.style.height = size + 'px';
 
   // durasi animasi random
   const duration = 5 + Math.random() * 5;
@@ -25,7 +29,7 @@ function createFlower() {
 }
 
 // bikin bunga terus muncul tiap 300ms
-setInterval(createFlower, 300);
+setInterval(createFlower, 500);
 
 window.addEventListener("beforeunload", () => {
   window.scrollTo(0, 0);
